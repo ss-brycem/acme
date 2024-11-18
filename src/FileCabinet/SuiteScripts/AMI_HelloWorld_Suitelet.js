f735cc46639a7e2edece4f4041dd9bde2589a993
Bryce Miller <brycem@solution-source.net>
2024-11-18 16:15:01 -0500

    change greeting

--- src/FileCabinet/SuiteScripts/AMI_HelloWorld_Suitelet.js
+++ src/FileCabinet/SuiteScripts/AMI_HelloWorld_Suitelet.js
@@ -15,7 +15,7 @@ define([], () => {
             name: 'Content-Type',
             value: 'text/plain',
         });
-        response.write('Hello, Nobody!');
+        response.write('Hello, Everybody!');
     };
 
     return { onRequest };
