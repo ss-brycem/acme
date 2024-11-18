77a3b85ae8c9e66803e1947af69e4fdf1e1687af
Bryce Miller <brycem@solution-source.net>
2024-11-18 15:37:30 -0500

    Working dev deploy (#1)
    * project template
    
    * tooling configs
    
    * packages
    
    * hello world suitelet
    
    * wip
    
    * clone via https
    
    * update suitelet
    
    * udpate suitelet again
    
    * update manifest
    
    * optimize deploy
    
    * fix deploy def
    
    * ok this should work now
    
    * greet noone
    
    * set deployment to DEBUG
    
    * update deployment

---
+++ src/FileCabinet/SuiteScripts/AMI_HelloWorld_Suitelet.js
@@ -0,0 +1,22 @@
+/**
+ * @NApiVersion 2.1
+ * @NScriptType Suitelet
+ */
+define([], () => {
+    /**
+     * Defines the Suitelet script trigger point.
+     * @param {Object} scriptContext
+     * @param {ServerRequest} scriptContext.request - Incoming request
+     * @param {ServerResponse} scriptContext.response - Suitelet response
+     * @since 2015.2
+     */
+    const onRequest = ({ response }) => {
+        response.addHeader({
+            name: 'Content-Type',
+            value: 'text/plain',
+        });
+        response.write('Hello, Nobody!');
+    };
+
+    return { onRequest };
+});
